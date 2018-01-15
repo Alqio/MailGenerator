d555d210c607c5c82e676778cb6901008e76399d
Alqio <kyosti.alkio@gmail.com>
2018-01-15 10:34:04 +0200

    Added another window for output text

--- src/gui/View.scala
+++ src/gui/View.scala
@@ -4,26 +4,39 @@ import scala.swing._
 import scala.swing.event._
 import javax.swing.UIManager
 import main._
+import java.io.File
+import javax.swing.JFileChooser
+import java.io.FileReader
+import scala.io.Source
 
 object View extends SimpleSwingApplication {
 	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
 
-	val mail = new Mail
+	var mail = new Mail
 
-	def top = new MainFrame {
+	val window = new Frame {
 		val output = new TextArea() {
-			rows = 10
+			rows = 40
 			columns = 80
 			editable = false
-			wordWrap = false
-			lineWrap = false
 		}
 		
 		output.text = mail.generateAll
 		
-		val flowpanel = new FlowPanel() {
+		contents = new FlowPanel {
 			contents += new ScrollPane(output)
 		}
+	}
+	
+	updateOutput()
+	
+	window.visible = true
+	
+	def updateOutput() = {
+		window.output.text = mail.generateAll
+	}
+	
+	val mainFrame = new MainFrame {
 		
 		val subtopicName  = new TextField(40)
 		val topic = new ComboBox(mail.topics)
@@ -33,8 +46,12 @@ object View extends SimpleSwingApplication {
 			editable = true
 		}
 		val addButton = new Button("Add subtopic")
+		val loadButton = new Button("Load")
+		val saveButton = new Button("Save")
 
-		val items = Array(subtopicName, topic, date, link, text, addButton)
+		
+		
+		val items = Array(subtopicName, topic, date, link, text, addButton, loadButton, saveButton)
 
 		items.foreach(this.listenTo(_))
 		
@@ -49,14 +66,57 @@ object View extends SimpleSwingApplication {
 					
 						mail.addSubtopicToTopic(topic.selection.item, subtopic)
 						
-						output.text = mail.generateAll
+						updateOutput()
 					}
-					
+				} else if (source == loadButton) {
+					val f = loadFile()
+					mail = Mail.createFromString(f)
+					updateOutput()
+				} else if (source == saveButton) {
+					saveFile()
 				}
 			}
 		}
 		
+
 		
+		def saveFile(): Option[File] = {
+			val workingDirectory = new File(System.getProperty("user.dir"));
+	
+			val saveFile = new JFileChooser()
+			saveFile.setCurrentDirectory(workingDirectory)
+			val ret = saveFile.showSaveDialog(null)
+			try {
+				val fl = saveFile.getSelectedFile()
+				if (fl == null) None else Some(fl)
+			} catch {
+				case ex: Exception => None
+			}
+
+		}
+
+		
+		def loadFile(): String = {
+			val workingDirectory = new File(System.getProperty("user.dir"));
+	
+			val openFile = new JFileChooser()
+			openFile.setCurrentDirectory(workingDirectory)
+			val ret = openFile.showOpenDialog(null)
+			
+			val file = try {
+				val fl = openFile.getSelectedFile()
+				if (fl == null) None else Some(fl)
+			} catch {
+				case ex: Exception => None
+			}
+			
+			if (file.isEmpty) return ""
+			
+			val path = file.get.getAbsolutePath()
+			
+			Source.fromFile(path).getLines.mkString
+					
+		}
 		
 		this.contents = new GridBagPanel { 
       import scala.swing.GridBagPanel.Anchor._
@@ -68,19 +128,15 @@ object View extends SimpleSwingApplication {
       layout += new Label("Text:")  -> new Constraints(0, 4, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
       
       layout += addButton						-> new Constraints(0, 5, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(8, 5, 5, 5), 0, 0)
+      layout += saveButton					-> new Constraints(1, 5, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(8, 5, 5, 5), 0, 0)
+      layout += loadButton					-> new Constraints(2, 5, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(8, 5, 5, 5), 0, 0)
 
-      layout += new Label("Output") -> new Constraints(0, 6, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(8, 5, 5, 5), 0, 0)
-      
-      layout += topic 							-> new Constraints(1, 0, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
-      layout += subtopicName				-> new Constraints(1, 1, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
-      layout += date  							-> new Constraints(1, 2, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
-      layout += link  							-> new Constraints(1, 3, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
-      layout += text  							-> new Constraints(1, 4, 1, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
-      layout += flowpanel 				  -> new Constraints(1, 6, 2, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
-      
-
+      layout += topic 							-> new Constraints(1, 0, 2, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
+      layout += subtopicName				-> new Constraints(1, 1, 2, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
+      layout += date  							-> new Constraints(1, 2, 2, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
+      layout += link  							-> new Constraints(1, 3, 2, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
+      layout += text  							-> new Constraints(1, 4, 2, 1, 0, 1, NorthWest.id, Fill.None.id, new Insets(5, 5, 5, 5), 0, 0)
     }
-		val j = new FlowPanel(output)
 		
 		this.menuBar = new MenuBar {
       contents += new Menu("Program") {
@@ -95,5 +151,8 @@ object View extends SimpleSwingApplication {
     
 		
 	}
+	def top = mainFrame
+	mainFrame.peer.setLocationRelativeTo(null)
+	window.peer.setLocationRelativeTo(mainFrame.peer)
 
 }
--- src/main/Mail.scala
+++ src/main/Mail.scala
@@ -11,45 +11,6 @@ class Mail {
   
   val topics = Vector(kalenteri, kilta, ayy, pohjanurkkaus)
   
-  
-  val sub1 = new Subtopic("UraTiKAS", Date(18,1))
-  val sub2 = new Subtopic("Tuxera Excu", Date(25,1), "tietokilta.fi")
-  val sub3 = new Subtopic("aTalent Excu", Date(15,1), "facebook.com")
-  sub1.text = """UraTiKAs tulee taas! 
-UraTiKAs järjestetään 18.1.2018 T-talolla salissa T1 alkaen kello 14.00. Sen aikana 12 kesätoitäkin 
-tarjoavaa firmaa esittelevät itsensä ja jokaisella firmalla on oma esittelypiste, jossa voi jutella 
-firman edustajien kanssa. """
-  sub2.text = """Join our Excu at Tuxera!
-
-Hey the Tietokilta members! We are pleased to invite you to our excursion on Wednesday 
-January 24 at 17:00 onwards. Come and learn on what Tuxera is about, see some of our 
-coding practices, and hear career stories from our employees. During the evening, 
-you will get a rare opportunity to visit our hardware-packed lab. 
-We will also talk about future opportunities for students interested in, for example, 
-Linux kernel, file systems, as well as storage and networking technologies.
-
-Food, drinks, Super Bomberman and table tennis will be all on the house! 
-You can also take advantage of our sauna and pool facilities if you wish. 
-In that case, just remember to bring your bathing suit and towel."""
-  sub3.text = """Are your LinkedIn profile and CV in top notch condition? Want to get some last minute 
-tips for UraTIKAs? aTalent welcomes TIK students to visit their homebase at Autotalo on 
-Tuesday 16.1. The idea is to take your job hunting tools and skills to the next level and 
-offer you a possibility to ask whatever you like from recruiters! Throughout the evening 
-there will be food, beverages and relaxed atmosphere to ensure a perfect venue to improve 
-your job hunting skills!
-
-During the night we’ll go through for example what is LinkedIn and how to get employers 
-interested about you even without active effort, what does it take to impress recruiters 
-with your CV and how to prepare yourself to UraTIKAs! Sign up to get your career boosted!
-
-So that we can check and give hints for your LinkedIn profile and / or your CV, 
-please bring your laptop and printed out CV!"""
-  
-  kilta.addSubtopic(sub1)
-  
-  kilta.addSubtopic(sub2)
-  kilta.addSubtopic(sub3)
-  
   def addSubtopicToTopic(topic: Topic, subtopic: Subtopic) = {
   	topic.addSubtopic(subtopic)
   }
@@ -75,4 +36,14 @@ please bring your laptop and printed out CV!"""
   	this.generateTableOfContents + generate
   }
   
+  
+}
+object Mail {
+	
+	def createFromString(str: String): Mail = {
+			
+		
+		???
+	}
+	
 }
