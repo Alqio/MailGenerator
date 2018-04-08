23b0f58c88af53ea95e0ca6d136458e49a6c422d
Kyösti Alkio <kyosti.alkio@gmail.com>
2018-04-08 20:45:06 +0300

    Modified html header

--- src/main/Mail.scala
+++ src/main/Mail.scala
@@ -60,22 +60,27 @@ class Mail {
     <meta charset="UTF-8">    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> 
-    
     <style>
         body {
             font-family: monospace;
+            border: 0px;
+            margin: 0px;
+            padding: 0px;
+            display: block;
+            font-size: 14px;
+            line-height: 150%;
+        }
+        pre {
+            white-space:pre-wrap;
+            white-space: -moz-pre-wrap;
+            white-space: -pre-wrap;
+            white-space: -o-pre-wrap;
+            word-wrap:break-word;
         }
     </style>
     
 </head>
+
 
 <body>
 <div class="container">
