f5eb61b708eec427ca09c6c788949c840f2ad4bc
Kyosti Alkio <kyosti.alkio@gmail.com>
2019-01-09 16:15:12 +0200

    Added basic loading of configuration that does not work yet

--- src/main/scala/main/global.scala
+++ src/main/scala/main/global.scala
@@ -1,11 +1,38 @@
 package main
 
+import scala.io.Source
+
 /**
   * TODO: Load these from config file
   */
 object global {
-  val topicChangeMark = "----"
-  val subtopicChangeMark = "---"
-  val year = 2019
-  val maxBackups = 3
+  //default values
+  var topicChangeMark = "----"
+  var subtopicChangeMark = "---"
+  var year = 2019
+  var maxBackups = 3
+
+  //loadConfiguration()
+
+  def loadConfiguration() = {
+
+    val configs = collection.mutable.Map[String, String]()
+
+    val bufferedSource = Source.fromFile("settings.cfg")
+    for (line <- bufferedSource.getLines) {
+      val attr = line.takeWhile(_ != '=').trim
+      val value = line.dropWhile(_ != '=').drop(1).trim
+      configs += attr -> value
+    }
+
+    bufferedSource.close
+
+    topicChangeMark = configs("topicChangeMark")
+    subtopicChangeMark = configs("subtopicChangeMark")
+    year = configs("year").asInstanceOf[Int]
+    maxBackups = configs("maxBackups").asInstanceOf[Int]
+    println("Loaded settings")
+  }
+
 }
+
